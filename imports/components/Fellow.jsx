dea484104d4767e557e362357b4818307a66ad34
Ben Obee <benobee@gmail.com>
2016-10-03 18:39:27 -0700

    formatting

--- imports/components/Fellow.jsx
+++ imports/components/Fellow.jsx
@@ -18,24 +18,24 @@ class Fellow extends React.Component {
         }
 
         return (
-		   	<div className="item fellow" 
+            <div className="item fellow" 
             
                 onClick={this.showModal.bind(this)} 
                 data-url={this.props.item.fullUrl} 
                 data-class={this.props.item.tag} 
                 data-region={this.props.item.category}>
 
-			    <div className="item-content">  
-			        <div className="item-thumbnail-wrapper">
-			            <div className="image" style={divStyle}></div>
-			        </div>
-			        <div className="location">{this.props.item.location}</div>
-			    </div>
-			    <div className="header">
-			        <div className="name">{this.props.item.name}</div>
-			        <div className="class">{this.props.item.category}</div>
-			    </div>
-		   	</div> 
+                <div className="item-content">  
+                    <div className="item-thumbnail-wrapper">
+                        <div className="image" style={divStyle}></div>
+                    </div>
+                    <div className="location">{this.props.item.location}</div>
+                </div>
+                <div className="header">
+                    <div className="name">{this.props.item.name}</div>
+                    <div className="class">{this.props.item.category}</div>
+                </div>
+            </div> 
         )
     }
 }
--- imports/components/FellowsList.jsx
+++ imports/components/FellowsList.jsx
@@ -10,117 +10,117 @@ class FellowsList extends React.Component {
         this.displayName = 'FellowsList';
         this.filter = {};
         this.state = {
-        	array: this.processArray(this.props.data.items),
-        	filter: ''
+            array: this.processArray(this.props.data.items),
+            filter: ''
         }
         this.fullArray = this.processArray(this.props.data.items);
     }
     processArray(array){
-		return _.map(array, (i) => {
-			
-				const category = _.first(i.categories);
+        return _.map(array, (i) => {
+            
+                const category = _.first(i.categories);
 
-				const tag = _.first(i.tags);
+                const tag = _.first(i.tags);
 
-        		const obj = {
-        			name: i.title,
-        			image: i.assetUrl,
-        			location: i.location.addressCountry,
-        			category: category,
-        			tag: tag,
-        			fullUrl: i.fullUrl,
-        			body: i.body,
+                const obj = {
+                    name: i.title,
+                    image: i.assetUrl,
+                    location: i.location.addressCountry,
+                    category: category,
+                    tag: tag,
+                    fullUrl: i.fullUrl,
+                    body: i.body,
                     issueArea: i.excerpt
-        		}
+                }
 
-        		return obj;
-       	});
+                return obj;
+        });
     }
     setFilters() {
         $(".filter select").on("change", (e) => {
 
-        	$('.fellow-list').removeClass("is-rendered");
+            $('.fellow-list').removeClass("is-rendered");
 
-        	const filter = e.currentTarget.value;
-        	const type = $(e.currentTarget).data("type");		
+            const filter = e.currentTarget.value;
+            const type = $(e.currentTarget).data("type");       
 
-			if (filter == "All"){	
-				delete this.filter[type];
-			} else {
-				this.filter[type] = filter;
-			}
+            if (filter == "All"){   
+                delete this.filter[type];
+            } else {
+                this.filter[type] = filter;
+            }
 
-        	this.runFilters();
+            this.runFilters();
             
         });
     }
     runFilters() {
-    	const search = this.filter;
-    	const results = _.where(this.fullArray, search);
-    	
-    	this.setState({array: results});
+        const search = this.filter;
+        const results = _.where(this.fullArray, search);
+        
+        this.setState({array: results});
     }
     componentDidUpdate() {
-		setTimeout(() => {
-        	$('.fellow-list').addClass("is-rendered");
+        setTimeout(() => {
+            $('.fellow-list').addClass("is-rendered");
         }, 400);
     }
     componentDidMount() {
-    	setTimeout(() => {
-			$('.fellow-list').addClass("is-rendered");
+        setTimeout(() => {
+            $('.fellow-list').addClass("is-rendered");
 
-			this.setFilters();
+            this.setFilters();
 
-    	}, 400);	
+        }, 400);    
     }
     renderResults(){
-    	if(this.state.array.length == 0){
-    		return <div><h2>No Results</h2></div>; 
-    	}
-		return this.state.array.map(( item, index ) => {
-			return( 
-				<Fellow item={item} key={index} />
-			);
-		});    	
+        if(this.state.array.length == 0){
+            return <div><h2>No Results</h2></div>; 
+        }
+        return this.state.array.map(( item, index ) => {
+            return( 
+                <Fellow item={item} key={index} />
+            );
+        });     
     }
     renderCategories(){
-		return this.props.data.collection.categories.map( (item, index) => {
-			return(
-				<option key={index} className="category item">{item}</option>
-			)
-		});    	
+        return this.props.data.collection.categories.map( (item, index) => {
+            return(
+                <option key={index} className="category item">{item}</option>
+            )
+        });     
     }
     renderTags(){
-    	return this.props.data.collection.tags.map( (item, index) => {
-			return(
-				<option key={index} className="tag item">{item}</option>
-			)
-		});
+        return this.props.data.collection.tags.map( (item, index) => {
+            return(
+                <option key={index} className="tag item">{item}</option>
+            )
+        });
     }
     render() {
         return(
-        	<div>
-				<div className="filters">
-				    <div className="categories filter">
-				      <div className="header">Class</div>
-				      <select className="items" data-type="category">
-				      		<option className="category item">All</option>
-				        	{this.renderCategories()}
-				      </select>
-				    </div>
-				    
-				    <div className="tags filter">
-				      <div className="header">Region</div>
-				      <select className="items" data-type="tag">
-				      		<option className="tag item">All</option>
-				        	{this.renderTags()}
-				      </select>
-				    </div>
-				</div>
-	        	<section className="fellow-list">
-					{this.renderResults()}
-				</section>
-			</div>
+            <div>
+                <div className="filters">
+                    <div className="categories filter">
+                      <div className="header">Class</div>
+                      <select className="items" data-type="category">
+                            <option className="category item">All</option>
+                            {this.renderCategories()}
+                      </select>
+                    </div>
+                    
+                    <div className="tags filter">
+                      <div className="header">Region</div>
+                      <select className="items" data-type="tag">
+                            <option className="tag item">All</option>
+                            {this.renderTags()}
+                      </select>
+                    </div>
+                </div>
+                <section className="fellow-list">
+                    {this.renderResults()}
+                </section>
+            </div>
         )
     }
 }
